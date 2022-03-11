b60aae635bfc6217eb2a8deb6019d03ae2a8c538
Akhiyarov Robert <r.akhiyarov@studiofact.ru>
2022-03-11 23:41:20 +0500

    feat: remove app controller and service && generate users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
 	imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
 			envFilePath: `.${process.env.NODE_ENV}.env`,
 		}),
 		MongooseModule.forRoot(process.env.MONGO_URL),
+		UsersModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
