e158aad6ae929027dd542801ba4c70840b170f00
Akhiyarov Robert <r.akhiyarov@studiofact.ru>
2022-03-11 23:32:28 +0500

    perf: migrate to fastify / fix: config save on prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(
-      {
-        envFilePath: `.${process.env.NODE_ENV}.env`
-      }
-    ), 
-    MongooseModule.forRoot(process.env.MONGO_URL)
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: `.${process.env.NODE_ENV}.env`,
+		}),
+		MongooseModule.forRoot(process.env.MONGO_URL),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import {
+	FastifyAdapter,
+	NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function start() {
-  const PORT = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
-  console.log(`Server listen on port ${PORT}`)
+	const PORT = process.env.PORT || 3000;
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter(),
+	);
+	await app.listen(PORT);
+	console.log(`Server listen on port ${PORT}`);
 }
 start();
