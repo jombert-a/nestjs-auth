2e4eb0b4d54f9d5050171d527f2e1c3fe98fbf6c
Akhiyarov Robert <r.akhiyarov@studiofact.ru>
2022-03-13 19:28:37 +0500

    feat: add jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '@/users/schemas/user.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
 	controllers: [AuthController],
 	providers: [AuthService],
-  imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-  ]
+	imports: [
+		MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+		JwtModule.register({ secret: 'test' }),
+	],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,47 @@
 import { User, UserDocument } from '@/users/schemas/user.schema';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { loginWithEmailDto } from './dto/login-with-email.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 	constructor(
 		@InjectModel(User.name) private userModel: Model<UserDocument>,
+		private readonly jwtService: JwtService,
 	) {}
 
+	async checkPassword(
+		password: string,
+		candidatePassword: string,
+	): Promise<void> {
+		const isMatch = await bcrypt.compare(password, candidatePassword);
+		if (!isMatch) {
+			throw new HttpException(
+				{
+					status: 0,
+					error: 'UNAUTHORIZED',
+				},
+				HttpStatus.UNAUTHORIZED,
+			);
+		}
+	}
+
+	createJsonWebToken(user: UserDocument): string {
+		return this.jwtService.sign({
+            id: user.id,
+            email: user.email,
+            username: user.username
+        });
+	}
+
 	async authWithEmailAndPassword(loginDto: loginWithEmailDto) {
-		const candidateToLogin: User = await this.userModel.findOne({
+		const candidateToLogin: UserDocument = await this.userModel.findOne({
 			email: loginDto.email,
 		});
-		const isMatch: boolean = await bcrypt.compare(
-			loginDto.password,
-			candidateToLogin.password,
-		);
-		return isMatch;
+		await this.checkPassword(loginDto.password, candidateToLogin.password);
+		return this.createJsonWebToken(candidateToLogin);
 	}
 }
