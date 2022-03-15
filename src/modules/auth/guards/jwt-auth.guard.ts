b356d9575b61026b2a55db49fc78b926d72d63d4
Akhiyarov Robert <nonrobertsbox@gmail.com>
2022-03-16 00:49:20 +0500

    fix: rewrite todo

--- src/modules/auth/guards/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -3,4 +3,4 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {}
-// TODO: разобраться что такое AuthGuard и как определять токен
+// TODO: разобраться что такое AuthGuard
